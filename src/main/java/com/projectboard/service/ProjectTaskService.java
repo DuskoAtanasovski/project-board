b8e446d453fbbb611de8877cf8281b972a2efd29
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-07-12 21:47:27 +0200

    adding files

--- src/main/java/com/projectboard/service/ProjectTaskService.java
+++ src/main/java/com/projectboard/service/ProjectTaskService.java
@@ -3,6 +3,7 @@ package com.projectboard.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projectboard.domain.ProjectTask;
 import com.projectboard.repository.ProjectTaskRepository;
 
 @Service
@@ -10,5 +11,14 @@ public class ProjectTaskService {
 
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
-	
+
+	public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+
+		if (projectTask.getStatus() == null || projectTask.getStatus() == "") {
+			projectTask.setStatus("TO_DO");
+		}
+
+		return projectTaskRepository.save(projectTask);
+	}
+
 }
--- src/main/java/com/projectboard/web/ProjectTaskController.java
+++ src/main/java/com/projectboard/web/ProjectTaskController.java
@@ -1,18 +1,30 @@
 package com.projectboard.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projectboard.domain.ProjectTask;
 import com.projectboard.service.ProjectTaskService;
 
 @RestController
 @RequestMapping("/api/board")
 @CrossOrigin
 public class ProjectTaskController {
-	
+
 	@Autowired
 	private ProjectTaskService projectTaskService;
+
+	@PostMapping("")
+	public ResponseEntity<?> addProjectTaskToBoard(@RequestBody ProjectTask projectTask) {
+
+		ProjectTask newPT = projectTaskService.saveOrUpdateProjectTask(projectTask);
+		return new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
+	}
 
 }
