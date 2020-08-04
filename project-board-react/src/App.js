d29aac91b2d6e1da6c3dacb227b4ebeda7173379
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-08-04 18:21:05 +0200

    Add existing file

--- project-board-react/src/App.js
+++ project-board-react/src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProjectBoard from './components/ProjectBoard';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
 class App extends Component {
   render(){
   return (
+    <Router>
     <div className="App">
     <Navbar/>
-    <ProjectBoard/>
+    <Route exact path="/" component={ProjectBoard} />
+    <Route exact path="/addProjectTask" component={AddProjectTask} />
     </div>
-  );}
+    </Router>
+  );
+}
 }
 
 export default App;
